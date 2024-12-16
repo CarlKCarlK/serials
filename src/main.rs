a889658d005ab69b7ca4a26e0130d94b2626f980
Carl Kadie <carlk@msn.com>
2024-12-16 09:11:20 -0800

    Finds fib

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,21 @@
 #![no_std]
 #![no_main]
 #![allow(clippy::future_not_send, reason = "Single-threaded")]
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+const HEAP_SIZE: usize = 1024 * 64; // in bytes
+
+use alloc_cortex_m::CortexMHeap;
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
+use embassy_time::{Duration, Instant, Timer};
 use lib::{
-    BlinkState, Blinker, BlinkerNotifier, Button, Clock, ClockNotifier, ClockState, Display,
-    DisplayNotifier, Never, Result,
-}; // This crate's own internal library
+    BlinkState, Blinker, BlinkerNotifier, Button, Display, DisplayNotifier, Never, Result, ONE_DAY,
+};
+use num_bigint::BigUint;
+// This crate's own internal library
 use panic_probe as _;
 
 #[embassy_executor::main]
@@ -20,20 +28,60 @@ pub async fn main(spawner0: Spawner) -> ! {
     panic!("{err}");
 }
 
-#[expect(clippy::items_after_statements, reason = "Keeps related code together")]
-async fn inner_main(spawner: Spawner) -> Result<Never> {
-    let hardware = lib::Hardware::default();
+#[expect(clippy::arithmetic_side_effects, reason = "cmk")]
+#[expect(unsafe_code, reason = "cmk")]
+async fn inner_main(_spawner: Spawner) -> Result<Never> {
+    unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
 
-    static CLOCK_NOTIFIER: ClockNotifier = Clock::notifier();
-    let mut clock = Clock::new(hardware.cells, hardware.segments, &CLOCK_NOTIFIER, spawner)?;
-    let mut button = Button::new(hardware.button);
-    info!("Clock and button created");
+    let start = Instant::now();
+    let mut low = 0;
+    let mut high = 1;
 
-    // Run the state machine
-    let mut state = ClockState::default();
+    // Exponential search to find an upper bound
+    while Instant::now().duration_since(start) < Duration::from_secs(1) {
+        let loop_start = Instant::now();
+        let result = fibonacci(high);
+        let elapsed = loop_start.elapsed();
+        info!(
+            "Fibonacci number at index {}: {} bits (computed in {})",
+            high,
+            result.bits(),
+            elapsed
+        );
+        if elapsed >= Duration::from_secs(1) {
+            break;
+        }
+        high *= 2;
+    }
+
+    // Binary search to find the largest Fibonacci number that can be generated in less than 1 second
+    while low < high {
+        #[expect(clippy::integer_division_remainder_used, reason = "cmk")]
+        let mid = (low + high) / 2;
+        let mid_start = Instant::now();
+        let result = fibonacci(mid);
+        let elapsed = mid_start.elapsed();
+        info!(
+            "Fibonacci number at index {}: {} bits (computed in {})",
+            mid,
+            result.bits(),
+            elapsed
+        );
+        if elapsed < Duration::from_secs(1) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+
+    info!(
+        "Largest Fibonacci number index that can be generated in less than one second: {}",
+        (low - 1)
+    );
+
+    // sleep forever
     loop {
-        defmt::info!("State: {:?}", state);
-        state = state.run_and_next(&mut clock, &mut button).await;
+        Timer::after(ONE_DAY).await;
     }
 }
 
@@ -81,4 +129,24 @@ async fn inner_main_blinky(spawner: Spawner) -> Result<Never> {
     }
 }
 
-// TODO: Is testing possible?
+#[expect(clippy::min_ident_chars, reason = "cmk")]
+#[expect(clippy::arithmetic_side_effects, reason = "cmk")]
+fn fibonacci(n: u64) -> BigUint {
+    if n == 0 {
+        return BigUint::from(0u64);
+    }
+    let mut a = BigUint::from(0u64);
+    let mut b = BigUint::from(1u64);
+    #[expect(clippy::integer_division_remainder_used, reason = "cmk")]
+    for _ in 0..((n - 1) / 2) {
+        a += &b;
+        b += &a;
+    }
+
+    if n & 1 == 0 {
+        // n is even
+        a + b
+    } else {
+        b
+    }
+}
--- useful.md
+++ useful.md
@@ -17,7 +17,7 @@ cargo doc --no-deps --open
 ```cmd
 python3 -m pip install -r C:\deldir\1124\Renode_RP2040\visualization\requirements.txt
 cd tests
-renode --console run_clock.resc
+renode --console run_fib.resc
 s
 ```
 
