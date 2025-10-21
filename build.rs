204d4ff0c345e05393cc4ab5fc99a32d9feec839
Carl Kadie <carlk@msn.com>
2025-10-20 19:31:34 -0700

    remove dst_offset from build.rs

--- build.rs
+++ build.rs
@@ -21,25 +21,16 @@ fn main() {
     let utc_offset = env::var("UTC_OFFSET_MINUTES")
         .expect("Missing UTC_OFFSET_MINUTES (set in ./.env or ~/.pico.env, e.g., -420 for PST)");
 
-    // Optional DST parameters
-    let dst_offset = env::var("DST_OFFSET_MINUTES").unwrap_or_else(|_| "0".to_string());
-    let dst_start = env::var("DST_START").unwrap_or_else(|_| "".to_string());
-    let dst_end = env::var("DST_END").unwrap_or_else(|_| "".to_string());
 
     // 4) Expose as compile-time constants
     println!("cargo:rustc-env=WIFI_SSID={ssid}");
     println!("cargo:rustc-env=WIFI_PASS={pass}");
     println!("cargo:rustc-env=UTC_OFFSET_MINUTES={utc_offset}");
-    println!("cargo:rustc-env=DST_OFFSET_MINUTES={dst_offset}");
-    println!("cargo:rustc-env=DST_START={dst_start}");
-    println!("cargo:rustc-env=DST_END={dst_end}");
 
     // Optional: don't rebuild unless these change
     println!("cargo:rerun-if-env-changed=WIFI_SSID");
     println!("cargo:rerun-if-env-changed=WIFI_PASS");
     println!("cargo:rerun-if-env-changed=UTC_OFFSET_MINUTES");
     println!("cargo:rerun-if-env-changed=DST_OFFSET_MINUTES");
-    println!("cargo:rerun-if-env-changed=DST_START");
-    println!("cargo:rerun-if-env-changed=DST_END");
     println!("cargo:rerun-if-changed=.env");
 }
