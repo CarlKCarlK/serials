3095f5b99bb2325e52c4bf3027a34122a49a06ec
Carl Kadie <carlk@msn.com>
2025-10-26 07:28:12 -0700

    refactor led strip: replace custom Rgb/frame with RGB8 alias and switch to per-pixel update API

--- examples/led_strip.rs
+++ examples/led_strip.rs
@@ -8,13 +8,12 @@ use embassy_time::Timer;
 use lib::{LedStrip, LedStripNotifier, Rgb, Result, LED_STRIP_LEN};
 use panic_probe as _;
 
-
+static LED_STRIP_NOTIFIER: LedStripNotifier = LedStrip::notifier();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
     let peripherals = embassy_rp::init(Default::default());
 
-    static LED_STRIP_NOTIFIER: LedStripNotifier = LedStrip::notifier();
     let mut led_strip = LedStrip::new(
         &LED_STRIP_NOTIFIER,
         peripherals.PIO1,
@@ -29,7 +28,9 @@ async fn main(spawner: Spawner) -> ! {
     let mut hue: u8 = 0;
 
     loop {
-        update_rainbow(&mut led_strip, hue).await.expect("pattern update failed");
+        update_rainbow(&mut led_strip, hue)
+            .await
+            .expect("pattern update failed");
 
         hue = hue.wrapping_add(3);
         Timer::after_millis(80).await;
@@ -39,7 +40,7 @@ async fn main(spawner: Spawner) -> ! {
 async fn update_rainbow(led_strip: &mut LedStrip, base: u8) -> Result<()> {
     for idx in 0..LED_STRIP_LEN {
         let offset = base.wrapping_add((idx as u8).wrapping_mul(16));
-        led_strip.set_pixel(idx, wheel(offset)).await?;
+        led_strip.update_pixel(idx, wheel(offset)).await?;
     }
     Ok(())
 }
@@ -47,12 +48,16 @@ async fn update_rainbow(led_strip: &mut LedStrip, base: u8) -> Result<()> {
 fn wheel(pos: u8) -> Rgb {
     let pos = 255 - pos;
     if pos < 85 {
-        Rgb::new(255 - pos * 3, 0, pos * 3)
+        rgb(255 - pos * 3, 0, pos * 3)
     } else if pos < 170 {
         let pos = pos - 85;
-        Rgb::new(0, pos * 3, 255 - pos * 3)
+        rgb(0, pos * 3, 255 - pos * 3)
     } else {
         let pos = pos - 170;
-        Rgb::new(pos * 3, 255 - pos * 3, 0)
+        rgb(pos * 3, 255 - pos * 3, 0)
     }
+}
+
+const fn rgb(r: u8, g: u8, b: u8) -> Rgb {
+    Rgb { r, g, b }
 }
--- src/led_strip.rs
+++ src/led_strip.rs
@@ -12,62 +12,21 @@ use smart_leds::RGB8;
 use crate::{Error, Result};
 
 pub const LED_STRIP_LEN: usize = 8;
+pub type LedStripDataPin = PIN_2;
+
+/// Re-export the RGB color representation used by the driver.
+pub type Rgb = RGB8;
 
 bind_interrupts!(struct LedStripIrqs {
     PIO1_IRQ_0 => InterruptHandler<PIO1>;
 });
 
-/// RGB color representation for the LED strip.
-#[derive(Clone, Copy, Default, PartialEq, Eq)]
-pub struct Rgb {
-    /// Red channel (0-255)
-    pub r: u8,
-    /// Green channel (0-255)
-    pub g: u8,
-    /// Blue channel (0-255)
-    pub b: u8,
-}
-
-impl Rgb {
-    /// Creates a new RGB value.
-    #[must_use]
-    pub const fn new(r: u8, g: u8, b: u8) -> Self {
-        Self { r, g, b }
-    }
-}
-
-impl From<Rgb> for RGB8 {
-    fn from(value: Rgb) -> Self {
-        RGB8 {
-            r: value.r,
-            g: value.g,
-            b: value.b,
-        }
-    }
-}
-
-impl From<RGB8> for Rgb {
-    fn from(value: RGB8) -> Self {
-        Self::new(value.r, value.g, value.b)
-    }
-}
-
-#[derive(Clone, Copy)]
-struct Frame(pub [Rgb; LED_STRIP_LEN]);
-
-impl Default for Frame {
-    fn default() -> Self {
-        Self([Rgb::default(); LED_STRIP_LEN])
-    }
-}
-
 /// Commands sent to the LED strip task.
 enum LedStripCommand {
-    Show(Frame),
+    Update { index: u8, color: Rgb },
 }
 
-type LedStripCommands =
-    EmbassyChannel<CriticalSectionRawMutex, LedStripCommand, 4>;
+type LedStripCommands = EmbassyChannel<CriticalSectionRawMutex, LedStripCommand, 8>;
 
 /// Notifier used to construct LED strip instances.
 pub struct LedStripNotifier {
@@ -91,7 +50,7 @@ impl LedStripNotifier {
 /// Virtual LED strip device handle.
 pub struct LedStrip {
     commands: &'static LedStripCommands,
-    frame: Frame,
+    pixels: [Rgb; LED_STRIP_LEN],
 }
 
 impl LedStrip {
@@ -104,79 +63,64 @@ impl LedStrip {
     /// Creates a new LED strip controller bound to the given notifier.
     pub fn new(
         notifier: &'static LedStripNotifier,
-        pio1: Peri<'static, PIO1>,
-        dma_ch1: Peri<'static, DMA_CH1>,
-        pin: Peri<'static, PIN_2>,
+        pio: Peri<'static, PIO1>,
+        dma: Peri<'static, DMA_CH1>,
+        pin: Peri<'static, LedStripDataPin>,
         spawner: Spawner,
     ) -> Result<Self> {
         spawner
-            .spawn(led_strip_driver(pio1, dma_ch1, pin, notifier.commands()))
+            .spawn(led_strip_driver(pio, dma, pin, notifier.commands()))
             .map_err(Error::TaskSpawn)?;
 
         Ok(Self {
             commands: notifier.commands(),
-            frame: Frame::default(),
+            pixels: [Rgb::default(); LED_STRIP_LEN],
         })
     }
 
-    /// Returns the color of the LED at `index`.
+    /// Returns the color of the LED at index.
     pub fn pixel(&self, index: usize) -> Result<Rgb> {
         if index >= LED_STRIP_LEN {
             return Err(Error::IndexOutOfBounds);
         }
-        Ok(self.frame.0[index])
+        Ok(self.pixels[index])
     }
 
     /// Updates a single LED and immediately pushes the change.
-    pub async fn set_pixel(&mut self, index: usize, color: Rgb) -> Result<()> {
+    pub async fn update_pixel(&mut self, index: usize, color: Rgb) -> Result<()> {
         if index >= LED_STRIP_LEN {
             return Err(Error::IndexOutOfBounds);
         }
 
-        self.frame.0[index] = color;
-        self.commit().await
-    }
-
-    /// Backwards-compatible alias for `update_pixel`.
-    pub async fn set(&mut self, index: usize, color: Rgb) -> Result<()> {
-        self.set_pixel(index, color).await
-    }
-
-    async fn commit(&self) -> Result<()> {
-        let frame = self.frame;
-        self.commands.send(LedStripCommand::Show(frame)).await;
+        self.pixels[index] = color;
+        self.commands
+            .send(LedStripCommand::Update {
+                index: index as u8,
+                color,
+            })
+            .await;
         Ok(())
-    }
-}
-
-impl core::ops::Index<usize> for LedStrip {
-    type Output = Rgb;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.frame.0[index]
     }
 }
 
 #[embassy_executor::task]
 async fn led_strip_driver(
-    pio1: Peri<'static, PIO1>,
-    dma_ch1: Peri<'static, DMA_CH1>,
-    pin: Peri<'static, PIN_2>,
+    pio: Peri<'static, PIO1>,
+    dma: Peri<'static, DMA_CH1>,
+    pin: Peri<'static, LedStripDataPin>,
     commands: &'static LedStripCommands,
 ) -> ! {
-    let mut pio = Pio::new(pio1, LedStripIrqs);
+    let mut pio = Pio::new(pio, LedStripIrqs);
     let program = PioWs2812Program::new(&mut pio.common);
-    let mut driver = PioWs2812::<PIO1, 0, LED_STRIP_LEN>::new(&mut pio.common, pio.sm0, dma_ch1, pin, &program);
-    let mut rgb_buffer = [RGB8::default(); LED_STRIP_LEN];
+    let mut driver = PioWs2812::<PIO1, 0, LED_STRIP_LEN>::new(&mut pio.common, pio.sm0, dma, pin, &program);
+    let mut frame = [Rgb::default(); LED_STRIP_LEN];
 
     loop {
-        let cmd = commands.receive().await;
-        match cmd {
-            LedStripCommand::Show(frame) => {
-                for (dst, src) in rgb_buffer.iter_mut().zip(frame.0.iter()) {
-                    *dst = (*src).into();
-                }
-                driver.write(&rgb_buffer).await;
+        match commands.receive().await {
+            LedStripCommand::Update { index, color } => {
+                let idx = usize::from(index);
+                frame[idx] = color;
+                driver.write(&frame).await;
             }
         }
     }
